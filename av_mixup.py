1bac4aaadaa8c2b531f561f02d96e1754c73cc1f
johncava <jcava@asu.edu>
2022-03-24 03:11:39 -0700

    changes to av_mixup

--- av_mixup.py
+++ av_mixup.py
@@ -2,6 +2,24 @@
 # AVMixup Implementation
 ##
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from dataset import *
+from torchvision import transforms
+
+import torchvision
+import torchvision.models as models
+import PIL
+
+##
+# Seed
+##
+import random
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
 from dataset import *
 
 ###
@@ -12,104 +30,93 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Model(nn.Module):
+###
+# Model Checking
+###
 
-    def __init__(self):
-        super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 5, (3,3))
-        self.conv2 = nn.Conv2d(5,5, (3,3))
-        self.pool1 = nn.MaxPool2d(4)
-        self.conv3 = nn.Conv2d(5, 10,(3,3))
-        self.conv4 = nn.Conv2d(10, 15, (3,3))
-        self.pool2 = nn.MaxPool2d(4)
-        self.conv5 = nn.Conv2d(15, 15, (3,3))
-        self.conv6 = nn.Conv2d(15, 20, (3,3))
-        self.pool3 = nn.MaxPool2d(4)
-        self.linear = nn.Linear(80, 43)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
+print('ADVERSARIAL MIXUP')
+model = models.resnet18(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
 
-        x = self.pool1(x)
-
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-
-        x = self.pool2(x)
-        
-        x = F.relu(self.conv5(x))
-        x = F.relu(self.conv6(x))
-        
-        x = self.pool3(x)
-        x = x.view(x.size()[0],-1)
-        x = self.linear(x)
-        return x
-
-gtsrb_dataset_train = GTSRB(root_dir='/scratch/jcava/GTSRB/GTSRB/Training')
-gtsrb_dataset_test = GTSRB(root_dir='/scratch/jcava/GTSRB/GTSRB/Training', training=False)
+# gtsrb_dataset_train = GTSRB(root_dir='/scratch/jcava/GTSRB/GTSRB/Training')
+# loader = torch.utils.data.DataLoader(gtsrb_dataset_train,
+#                                              batch_size=len(gtsrb_dataset_train), shuffle=True,
+#                                              num_workers=8)
+# data = next(iter(loader))
+# mean, std = data[0].mean(), data[0].std()
+# print(mean,std)
 
 batch_size = 128
+gtsrb_dataset_train = GTSRBImbalance(root_dir='/scratch/jcava/GTSRB/GTSRB/Training', minority=14, training=True,
+                                            transform=transforms.Compose([transforms.RandomApply([
+                                                transforms.RandomRotation(20, resample=PIL.Image.BICUBIC),
+                                                transforms.RandomAffine(0, translate=(0.2, 0.2),
+                                                                        resample=PIL.Image.BICUBIC),
+                                                transforms.RandomAffine(0, shear=20, 
+                                                                        resample=PIL.Image.BICUBIC),
+                                                transforms.RandomAffine(0, scale=(0.8, 1.2), 
+                                                                        resample=PIL.Image.BICUBIC)
+                                            ]),
+                                            transforms.ToTensor()]))
 dataset_loader = torch.utils.data.DataLoader(gtsrb_dataset_train,
                                              batch_size=batch_size, shuffle=True,
                                              num_workers=8)
 
+gtsrb_dataset_test = GTSRBImbalance(root_dir='/scratch/jcava/GTSRB/GTSRB/Training', minority=14, training=False)
+
 test_dataset = torch.utils.data.DataLoader(gtsrb_dataset_test,
                                              batch_size=1, shuffle=True,
                                              num_workers=8)
+
 ###
 # Initial Training
 ###
-
-model = Model().half().cuda()
+model = model.cuda()
 criterion = nn.CrossEntropyLoss()
+from advertorch.attacks import GradientSignAttack
+adversary = GradientSignAttack(model, loss_fn=criterion, eps=0.3, clip_min=0.0, clip_max=1.0, targeted=False)
+
 import torch.optim as optim
-optimizer = optim.Adam(model.parameters(), lr=1e-2)
-max_epochs = 100
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+max_epochs = 16
 import time
 from tqdm import tqdm
-# print(len(dataset_loader))
-# for epoch in range(max_epochs):
-#     start = time.time()
-#     for i, (x,y) in tqdm(enumerate(dataset_loader)):
-#         x = x.half().cuda()
-#         y = y.cuda()
-#         pred = model(x)
-#         optimizer.zero_grad()
-#         loss = criterion(pred, y)
-#         loss.backward()
-#         optimizer.step()
-#         break
-#     end = time.time()
-#     print('Epoch ' + str(epoch) + ': ' + str(end-start) + 's')
+print(len(dataset_loader))
+loss_iteration = []
 
-
-from advertorch.attacks import GradientSignAttack
-
-# adversary = LinfPGDAttack(
-#     model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=0.3,
-#     nb_iter=40, eps_iter=0.01, rand_init=True, clip_min=0.0, clip_max=1.0,
-#     targeted=False)
-
-from losses import AlphaLoss
-
-loss_fn = AlphaLoss(classes=43, params={'alpha' : 1.0})
-
-from advertorch.attacks import LinfPGDAttack
-
-adversary = LinfPGDAttack(
-    model, loss_fn=loss_fn, eps=0.3,
-    nb_iter=40, eps_iter=0.01, rand_init=True, clip_min=0.0, clip_max=1.0,
-    targeted=False)
-
-# adversary = GradientSignAttack(model, loss_fn=loss_fn, eps=0.3, clip_min=0.0, clip_max=1.0, targeted=False)
+###
+# Base Training
+###
+for epoch in range(max_epochs):
+    start = time.time()
+    loss_iteration_base = []
+    # Regular Training
+    for i, (x,y) in tqdm(enumerate(dataset_loader)):
+        x = x.cuda()
+        y = y.cuda()
+        pred = model(x)
+        optimizer.zero_grad()
+        loss = criterion(pred, y)
+        # print(torch.isnan(x).any())
+        loss_iteration_base.append(loss.item())
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
+        optimizer.step()
+    loss_iteration.append(np.mean(loss_iteration_base))
+    end = time.time()
+    print('Epoch ' + str(epoch) + ': ' + str(end-start) + 's')
 
 ###
 # Adversarial Training
 ###
-print(len(dataset_loader))
 for epoch in range(max_epochs):
     start = time.time()
+    loss_iteration_adv = []
     for i, (x,y) in tqdm(enumerate(dataset_loader)):
         x = x.half().cuda()
         y = y.cuda()
@@ -121,22 +128,90 @@ for epoch in range(max_epochs):
         optimizer.zero_grad()
         loss = criterion(pred, y)
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
         optimizer.step()
+    loss_iteration.append(np.mean(loss_iteration_adv))
     end = time.time()
     print('Epoch ' + str(epoch) + ': ' + str(end-start) + 's')
 
+
+##
+# Plot
+##
+plt.figure()
+plt.plot(list(range(len(loss_iteration))), loss_iteration)
+plt.savefig('adv_mixup_resnet18_imbalance_loss.png')
+
 ###
-# Testing
+# AT Testing
 ###
+model.eval()
 print(len(test_dataset))
 acc = 0
+predictions = []
+true = []
 for i, (x,y) in tqdm(enumerate(test_dataset)):
-    x = x.half().cuda()
+    x = x.cuda()
     y = y.cuda()
     pred = model(x)
-    pred = torch.argmax(pred.squeeze(0)).item()
+    pred = torch.argmax(pred,dim=1).item()
     # print(pred.item(), y.squeeze(0).item())
-    if pred == y.squeeze(0).item():
+    # print(pred)
+    predictions.append(pred)
+    true.append(y.item())
+    if pred == y.item():
         acc += 1
-print('Adversarial Mixup Training Accuracy: ' + str(float(acc/len(test_dataset))))
+print('Adversarial Training Accuracy: ' + str(float(acc/len(test_dataset))))
+
+from sklearn.metrics import confusion_matrix
+
+import seaborn as sns
+
+plt.figure()
+
+cf_matrix = confusion_matrix(true, predictions)
+ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='.5f')
+
+ax.set_title('AT Confusion Matrix\n\n');
+ax.set_xlabel('\nPredicted Values')
+ax.set_ylabel('Actual Values ');
+
+## Ticket labels - List must be in alphabetical order
+ax.xaxis.set_ticklabels(['Minority','Majority'])
+ax.yaxis.set_ticklabels(['Minority','Majoirty'])
+
+## Display the visualization of the Confusion Matrix.
+plt.savefig('adv-mixup-confusion-matrix.png')
+
+adv_acc = 0
+predictions = []
+true = []
+for i, (x,y) in tqdm(enumerate(test_dataset)):
+    x = x.cuda()
+    y = y.cuda()
+    adv_untargeted = adversary.perturb(x, y)
+    pred = model(adv_untargeted)
+    pred = torch.argmax(pred,dim=1).item()
+    # print(pred.item(), y.squeeze(0).item())
+    # print(pred)
+    predictions.append(pred)
+    true.append(y.item())
+    if pred == y.item():
+        adv_acc += 1
+print('Adversarial Attack Accuracy: ' + str(float(adv_acc/len(test_dataset))))
+
+plt.figure()
+cf_matrix = confusion_matrix(true, predictions)
+ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='.5f')
+
+ax.set_title('AT Adversarial Attack Confusion Matrix\n\n');
+ax.set_xlabel('\nPredicted Values')
+ax.set_ylabel('Actual Values ');
+
+## Ticket labels - List must be in alphabetical order
+ax.xaxis.set_ticklabels(['Minority','Majority'])
+ax.yaxis.set_ticklabels(['Minority','Majoirty'])
+
+## Display the visualization of the Confusion Matrix.
+plt.savefig('adv-mixup-adversarial-attack-confusion-matrix.png')
 print('Done')
